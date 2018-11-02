159fb2e5fb922a7f647be33aa2a8afaeec75c7cd
Ben Shi <ben@hbish.com>
2018-11-03 00:44:39 +1100

    initial mastodon plugin

---
+++ schema.js
@@ -0,0 +1,81 @@
+const {
+  GraphQLObjectType,
+  GraphQLBoolean,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLEnumType,
+  GraphQLList
+} = require(`gatsby/graphql`);
+
+const ApplicationType = new GraphQLObjectType({
+  name: 'MastodonApplication',
+  fields: {
+    name: { type: GraphQLString },
+    website: { type: GraphQLString }
+  }
+});
+
+const AccountType = new GraphQLObjectType({
+  name: 'MastodonUser',
+  fields: {
+    id: { type: GraphQLFloat },
+    username: { type: GraphQLString },
+    acct: { type: GraphQLString },
+    display_name: { type: GraphQLString },
+    locked: { type: GraphQLBoolean },
+    bot: { type: GraphQLBoolean },
+    created_at: { type: GraphQLString },
+    note: { type: GraphQLString },
+    url: { type: GraphQLString },
+    avatar: { type: GraphQLString },
+    avatar_static: { type: GraphQLString },
+    header: { type: GraphQLString },
+    header_static: { type: GraphQLString },
+    followers_count: { type: GraphQLInt },
+    following_count: { type: GraphQLInt },
+    statuses_count: { type: GraphQLInt }
+  }
+});
+
+exports.tootType = {
+  created_at: {
+    type: GraphQLString
+  },
+  content: {
+    type: GraphQLString
+  },
+  url: {
+    type: GraphQLString
+  },
+  reblogs_count: {
+    type: GraphQLInt
+  },
+  favourites_count: {
+    type: GraphQLInt
+  },
+  replies_count: {
+    type: GraphQLInt
+  },
+  visibility: {
+    type: GraphQLString
+  },
+  sensitive: {
+    type: GraphQLBoolean
+  },
+  favourited: {
+    type: GraphQLBoolean
+  },
+  reblogged: {
+    type: GraphQLBoolean
+  },
+  muted: {
+    type: GraphQLBoolean
+  },
+  account: {
+    type: AccountType
+  },
+  application: {
+    type: ApplicationType
+  }
+};
---
+++ src/gatsby-node.js
@@ -0,0 +1,53 @@
+const Masto = require('mastodon')
+
+const { tootType } = require('./schema')
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  configOptions
+) => {
+  const { createNode } = actions
+
+  // Gatsby adds a configOption that's not needed for this plugin, delete it
+  delete configOptions.plugins
+
+  var M = new Masto({
+    access_token: configOptions.access_token,
+    timeout_ms: 60 * 1000,
+    api_url: configOptions.api_url,
+  })
+
+  const createNodes = toots => {
+    toots.forEach(toot => {
+      const nodeId = createNodeId(`toot-${toot.id}`)
+      const nodeContent = JSON.stringify(toot)
+
+      const nodeData = Object.assign({}, toot, {
+        id: nodeId,
+        parent: `__SOURCE__`,
+        children: [],
+        internal: {
+          type: `Toot`,
+          content: nodeContent,
+          contentDigest: createContentDigest(toot),
+        },
+      })
+
+      createNode(nodeData)
+    })
+  }
+
+  return M.get(`accounts/${configOptions.user_id}/statuses`, {
+    exclude_replies: true,
+    limit: configOptions.limit,
+  }).then(resp => {
+    createNodes(resp.data)
+  })
+}
+
+exports.setFieldsOnGraphQLNodeType = ({ type }) => {
+  if (type.name !== `Toot`) {
+    return {}
+  }
+  return tootType
+}
